cf373c6d14d246d13c07430a766332cf917f0300
Joe Prollins <joe.prollins@hotmail.com>
2022-07-24 01:13:16 +0100

    added transition, home and name entry scenes

--- Game/OtherScenes/Home_Screen.gd
+++ Game/OtherScenes/Home_Screen.gd
@@ -4,7 +4,7 @@ func _ready():
 	pass
 
 func _on_Button_New_Game_pressed():
-	get_tree().change_scene("res://OtherScenes/Name_Entry.tscn")
+	CHANGE_SCENE.change_scene("res://OtherScenes/Name_Entry.tscn")
 	pass # Replace with function body.
 
 func _on_Button_Continue_Game_pressed():
--- Game/OtherScenes/Name_Entry.gd
+++ Game/OtherScenes/Name_Entry.gd
@@ -6,8 +6,8 @@ onready var textbox = $Background/Textbox
 func _ready():
 	pass 
 
-
 func _on_Button_Confirm_pressed():
 	player_name = textbox.get_text()
 	print(player_name)
+	print("To implement next sections")
 	pass # Replace with function body.
